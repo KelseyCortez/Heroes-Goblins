e5f0e764893916f3f5ffb3d039597cd8e3455661
KelseyCortez <kelscortez@gmail.com>
2020-04-10 09:02:49 -0400

    updated files

--- goblin.py
+++ goblin.py
@@ -1,18 +1,18 @@
-# import Character from Character
+from character import Character
 
-# class Goblin(Character):
-#     pass
-    # def __init__(self, goblin_name, goblin_health, goblin_power):
-    #     self.name = goblin_name
-    #     self.health = goblin_health
-    #     self.power = goblin_power
-    #     # self.goblin_attack = goblin_attack
+class Goblin(Character):
+    pass
+    def __init__(self, goblin_name, goblin_health, goblin_power):
+        self.name = goblin_name
+        self.health = goblin_health
+        self.power = goblin_power
+        # self.goblin_attack = goblin_attack
 
-    # def attack(self, target):
-    #     print(f'{self.name} is attacking {target.name}')
+    def attack(self, target):
+        print(f'{self.name} is attacking {target.name}')
 
-    # def alive(self):
-    #     if self.health > 0:
-    #         return True
-    #     else:
-    #         return False
+    def alive(self):
+        if self.health > 0:
+            return True
+        else:
+            return False
--- superhero.py
+++ superhero.py
@@ -1,19 +1,19 @@
-# import Character from Character
+from character import Character
 
-# class Hero(Character):
-#     pass
-    # def __init__(self, hero_name, hero_health, hero_power):
-    #     self.name = hero_name
-    #     self.health = hero_health
-    #     self.power = hero_power
-    #     # self.attack_name = hero_attack
+class Hero(Character):
+    pass
+    def __init__(self, hero_name, hero_health, hero_power):
+        self.name = hero_name
+        self.health = hero_health
+        self.power = hero_power
+        # self.attack_name = hero_attack
         
-    # def attack(self, target):
-    #     print(f'{self.name} is attacking {target.name}')
+    def attack(self, target):
+        print(f'{self.name} is attacking {target.name}')
 
-    # def alive(self):
-    #     if self.health > 0:
-    #         return True
-    #     else:
-    #         return False
+    def alive(self):
+        if self.health > 0:
+            return True
+        else:
+            return False
 
